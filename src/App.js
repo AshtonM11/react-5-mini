73a92feee7ef2341b3cc1a37c4cd9b0602ad2a52
Ashton Morris <ashtonmorris14@gmail.com>
2018-06-19 20:07:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,65 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
+          <h1 className="counter__current-value">{0}</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={ () => null }
+              onClick={() => null}
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={ () => null }
+              onClick={() => null}
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={ () => null }
+              onClick={() => null}
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={ () => null }
+              onClick={() => null}
             >
               -5
             </button>
             <br />
             <button
               className="counter__button undo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={true}
+              onClick={() => null}
             >
               Undo
             </button>
             <button
               className="counter__button redo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={true}
+              onClick={() => null}
             >
               Redo
             </button>
           </div>
         </section>
         <section className="state">
-          <pre>
-            { JSON.stringify( this.props, null, 2 ) }
-          </pre>
+          <pre>{JSON.stringify(this.props, null, 2)}</pre>
         </section>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps)(App);
